ffe413b409843e707c5eebe648c9db828d1aab26
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-08 23:19:20 +0530

    Successfully connected or disconnected from server

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.get("/", express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected !", socket.id);
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+  setInterval(() => {
+    socket.emit("from server");
+  }, 1000);
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
